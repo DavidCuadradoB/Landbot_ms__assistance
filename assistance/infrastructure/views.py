560466b0f56f9c14cb038b545884edace53ad954
DavidCuadradoBallesteros <davidcuadradoballesteros@gmail.com>
2023-04-14 22:03:24 +0200

    Create a test post request.

--- assistance/infrastructure/views.py
+++ assistance/infrastructure/views.py
@@ -1,17 +1,27 @@
 from dependency_injector.wiring import inject, Provide
 from django.http import HttpResponse, JsonResponse, HttpRequest
+from django.views.decorators.csrf import csrf_exempt
 
 from assistance.application.EventService import EventService
 from assistance.infrastructure.containers import Container
 
 
 @inject
+@csrf_exempt
 def index(request: HttpRequest,
           event_service: EventService = Provide[Container.event_service]
           ):
-    event_service.print_my_name()
-    data = {
-        'name': 'David',
-        'location': 'Barna'
-    }
-    return JsonResponse(data)
+    print(request)
+    if request.method == 'POST':
+        print(request.body)
+        data = {
+            'name': 'Post',
+        }
+        return JsonResponse(data)
+    else:
+        event_service.print_my_name()
+        data = {
+            'name': 'David',
+            'location': 'Barna'
+        }
+        return JsonResponse(data)
